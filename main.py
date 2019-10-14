8e640d781ecda3fa0c754e90c78d4cd1dfc1395c
Maxim Garmash <maxim.garmash@gmail.com>
2019-10-14 19:31:46 +0300

    Fix problem with magic library

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import magic
 
+
+file = u'Книга'
 # file_magic = magic.Magic(magic_file="D:\Distr\Python\Projects\EnglishEasyCheckBot\env\Scripts")
-print(magic.from_file('O`Reilly', mime=True))
+print(magic.from_file(file, mime=True))
