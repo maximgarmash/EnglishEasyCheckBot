d33bffaa6f80872378dba9be7e11e8a88b97ce3d
Maxim Garmash <maxim.garmash@gmail.com>
2019-10-19 13:14:56 +0300

    Import fleep

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-import magic
+import fleep
 
 
-file = u'Книга'
-# file_magic = magic.Magic(magic_file="D:\Distr\Python\Projects\EnglishEasyCheckBot\env\Scripts")
-print(magic.from_file(file, mime=True))
+file_pdf = 'Книга'
+file_jpg = 'photo_jpg'
+file_mp3 = 'single_mp3'
+file_png = 'logo_png'
+file_json = 'answer_json'
+with open(file_jpg, 'rb') as f:
+    info = fleep.get(f.read(128))
+
+print(info.type)
+print(info.extension)
+print(info.mime)
+
+print(info.type_matches("raster-image"))
+print(info.extension_matches("gif"))
+print(info.mime_matches("image/png"))
