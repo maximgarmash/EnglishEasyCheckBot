29d5bbbfe73f2f52653042c475746968d38f5d5c
Maxim Garmash <maxim.garmash@gmail.com>
2019-10-24 17:56:27 +0300

    parse input file

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import fleep
+filename = 'english-module.txt'
+
+eng_dict = {}
+with open(filename, 'r', encoding='utf-8') as file:
+    for line in file.read().splitlines():
+        eng_dict[line.split('\t')[0]] = line.split('\t')[1]
+print(eng_dict)
 
 
-file_pdf = 'Книга'
-file_jpg = 'photo_jpg'
-file_mp3 = 'single_mp3'
-file_png = 'logo_png'
-file_json = 'answer_json'
-with open(file_jpg, 'rb') as f:
-    info = fleep.get(f.read(128))
 
-print(info.type)
-print(info.extension)
-print(info.mime)
 
-print(info.type_matches("raster-image"))
-print(info.extension_matches("gif"))
-print(info.mime_matches("image/png"))
+# import fleep
+    # info = fleep.get(f.read(128))
+#
+# print(info.type)
+# print(info.extension)
+# print(info).mime)
+#
+# print(info.type_matches("raster-image"))
+# print(info.extension_matches("gif"))
+# print(info.mime_matches("image/png"))
